f076122a6a88dcf7886a4ac563f2e3e57f10ae99
Sailaja Nandela <snandyala0@gmail.com>
2018-06-29 13:36:25 -0500

    final commit

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// intiating the menu and parallax container
+$(document).ready(function () {
+    // Custom JS & JQuery here
+    // $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+  })
